cf2de756cfe3b196f23d174d673a837ef72ee9d7
NguyenTrungHieu1999 <NguyenTrungHieu1999@users.noreply.github.com>
2019-10-07 20:04:01 +0700

    Website bán sách

---
+++ DA_CuoiKy_TKWeb/CacTrangWeb/FileScript/scrip.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function(){
+	i=1;
+	$("#cong").click(function () {
+        	if(i<100){
+        		$("#qty").attr("value", i+=1);
+		}
+        });
+	$("#tru").click(function(){
+        	if(i!=1){
+              		$("#qty").attr({"value": i-=1});
+		}
+	});
+	$("#dangky").click(function () {
+              	if($("#email").val()=="" || !$("#email").val().match('@'))
+        		alert("Email không hợp lệ. Mời bạn nhập lại!");
+              	else
+                	alert("Đăng ký thành công!");
+        });
+        var j=document.getElementById("qty").value;
+	$("#mua").click(function () {
+              	$("#soluong").text(i);
+        });
+});		
